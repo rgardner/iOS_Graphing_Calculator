59346383ce2d8dda3901a48ecfa1df9cd9c84213
Robert Gardner <rhg259@nyu.edu>
2013-01-06 23:30:59 -0800

    Added empty GraphView files

---
+++ GraphingCalculator/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  GraphingCalculator
+//
+//  Created by Bob Gardner on 1/6/13.
+//  Copyright (c) 2013 edu.stanford.cs193p.rhg259. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
---
+++ GraphingCalculator/GraphView.m
@@ -0,0 +1,31 @@
+//
+//  GraphView.m
+//  GraphingCalculator
+//
+//  Created by Bob Gardner on 1/6/13.
+//  Copyright (c) 2013 edu.stanford.cs193p.rhg259. All rights reserved.
+//
+
+#import "GraphView.h"
+
+@implementation GraphView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
