f272dcfea904e5d0b67228f604b46d7c7c00b1f9
Robert Gardner <rhg259@nyu.edu>
2013-01-12 15:08:30 -0800

    Adds button to splitViewController in Portrait using protocol

--- GraphingCalculator/RotatableViewController.m
+++ GraphingCalculator/RotatableViewController.m
@@ -7,12 +7,50 @@
 //
 
 #import "RotatableViewController.h"
+#import "SplitViewBarButtonItemPresenterProtocol.h"
 
 @interface RotatableViewController ()
 
 @end
 
 @implementation RotatableViewController
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.splitViewController.delegate = self;
+}
+
+- (id <SplitViewBarButtonItemPresenterProtocol>) splitViewBarButtonItemPresenter {
+    id detailVC = [self.splitViewController.viewControllers lastObject];
+    if (![detailVC conformsToProtocol:@protocol(SplitViewBarButtonItemPresenterProtocol)]) {
+        detailVC = nil;
+    }
+    return detailVC;
+}
+
+
+- (BOOL)splitViewController:(UISplitViewController *)svc
+   shouldHideViewController:(UIViewController *)vc
+              inOrientation:(UIInterfaceOrientation)orientation {
+    
+    return [self splitViewBarButtonItemPresenter] ? UIInterfaceOrientationIsPortrait(orientation) : NO;
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+     willHideViewController:(UIViewController *)aViewController
+          withBarButtonItem:(UIBarButtonItem *)barButtonItem
+       forPopoverController:(UIPopoverController *)pc {
+    
+    barButtonItem.title = self.title;
+    [self splitViewBarButtonItemPresenter].splitViewBarButtonItem = barButtonItem;
+}
+
+-(void)splitViewController:(UISplitViewController *)svc
+    willShowViewController:(UIViewController *)aViewController
+ invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem {
+
+    [self splitViewBarButtonItemPresenter].splitViewBarButtonItem = nil;
+}
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
     return YES;
