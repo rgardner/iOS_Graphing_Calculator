71540db7dba1f6d4521b53570ef263ed4bb39a29
Robert Gardner <rhg259@nyu.edu>
2013-01-12 14:53:02 -0800

    Added stubbed rotatableviewcontroller files. No storyboard item uses them

---
+++ GraphingCalculator/RotatableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RotatableViewController.h
+//  GraphingCalculator
+//
+//  Created by Bob Gardner on 1/12/13.
+//  Copyright (c) 2013 edu.stanford.cs193p.rhg259. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatableViewController : UIViewController
+
+@end
---
+++ GraphingCalculator/RotatableViewController.m
@@ -0,0 +1,21 @@
+//
+//  RotatableViewController.m
+//  GraphingCalculator
+//
+//  Created by Bob Gardner on 1/12/13.
+//  Copyright (c) 2013 edu.stanford.cs193p.rhg259. All rights reserved.
+//
+
+#import "RotatableViewController.h"
+
+@interface RotatableViewController ()
+
+@end
+
+@implementation RotatableViewController
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    return YES;
+}
+
+@end
